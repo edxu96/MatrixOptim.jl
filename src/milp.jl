79f56b3121c9480d19a2b992857f8338b5da16f0
edxu96 <edxu96@outlook.com>
2019-08-12 13:49:14 +0200

    update

--- src/milp.jl
+++ src/milp.jl
@@ -3,10 +3,12 @@
 # Author: Edward J. Xu, edxu96@outlook.com
 # Date: August 11, 2019
 
+
 @enum ColRow begin
    row = 1
    col = 2
 end
+
 
 function checkColVec(vec::Array{Int64,2}, str_name::String)
     if size(vec)[2] == 1
@@ -15,11 +17,22 @@ function checkColVec(vec::Array{Int64,2}, str_name::String)
     return
 end
 
+
 function checkMatrixMatch(array_1::Array{Int64,2}, array_2::Array{Int64,2}, whi_1::ColRow, whi_2::ColRow,
     str_name_1::String, str_name_2::String)
     if size(array_1)[Int(whi_1)] != size(array_2)[Int(whi_2)]
         throw("The $(str(whi_1)) of $str_name_1 doesn't match the $(str(whi_2)) of $str_name_2.")
     end
+end
+
+
+"""
+Solution for Model
+"""
+mutable struct Solution
+    obj
+    vec_result_x
+    vec_result_u
 end
 
 """
@@ -31,6 +44,7 @@ mutable struct ModelMix
     vec_f    # the coefficient vector for integer variables in the objective function
     mat_bCap
     vec_b
+    solution::Union{Solution, Missing}
 
     function ModelMix(vec_c::Array{Int64,2}, mat_aCap::Array{Int64,2}, vec_b::Array{Int64,2}, vec_f::Array{Int64,2},
         mat_bCap::Array{Int64,2})
@@ -41,9 +55,10 @@ mutable struct ModelMix
         checkMatrixMatch(vec_f, mat_bCap, row, col, "vec_f". "mat_bCap")
         checkMatrixMatch(mat_aCap, mat_bCap, row, row, "mat_aCap". "mat_bCap")
         checkMatrixMatch(mat_aCap, vec_b, row, row, "mat_aCap". "vec_b")
-        new(vec_c, mat_aCap, vec_f, mat_bCap, vec_b)
+        new(vec_c, mat_aCap, vec_f, mat_bCap, vec_b, missing)
     end
 end
+
 
 """
 Model for Linear Programming
@@ -58,10 +73,14 @@ mutable struct ModelLinear
         checkColVec(vec_b, "vec_b")
         checkMatrixMatch(vec_c, mat_aCap, row, col, "vec_c". "mat_aCap")
         checkMatrixMatch(mat_aCap, vec_b, row, row, "mat_aCap". "vec_b")
-        new(vec_c, mat_aCap, vec_b)
+        new(vec_c, mat_aCap, vec_b, missing)
     end
 end
 
+
+"""
+Get the model for optimization
+"""
 function getModelProgramming(vec_c::Array{Int64,2}, mat_aCap::Array{Int64,2}, vec_b::Array{Int64,2},
     vec_f::Union{Missing, Array{Int64,2}}=missing, mat_bCap::Union{Missing, Array{Int64,2}}=missing)
     if (vec_f === missing) & (mat_bCap === missing)
@@ -72,6 +91,7 @@ function getModelProgramming(vec_c::Array{Int64,2}, mat_aCap::Array{Int64,2}, ve
         throw("Input `vec_f` or `mat_bCap` do not match.")
     end
 end
+
 
 """
 Append a constraint to ModelMixed
@@ -85,8 +105,9 @@ function appendConstraint(model::ModelMixed, vec_a_new::Array{Int64,2}, vec_b_ne
     return model
 end
 
+
 """
-    lp(n_x, vec_c, vec_b, mat_a)
+    solveLinear(n_x, vec_c, vec_b, mat_a)
 
 Linear Programming in Matrix Form
 
@@ -95,7 +116,7 @@ Linear Programming in Matrix Form
 - vec_b: [column vector] right-hand-side coefficient vector in constraint
 - mat_a: Coefficient Matrix
 """
-function lp(n_x, vec_c, vec_b, mat_a)
+function solveLinear(n_x, vec_c, vec_b, mat_a)
     model = Model(solver = GLPKSolverLP())
     @variable(model, vec_x[1: n_x] >= 0)
     @objective(model, Min, (transpose(vec_c) * vec_x)[1])
@@ -107,13 +128,14 @@ function lp(n_x, vec_c, vec_b, mat_a)
     return obj, vec_result_x, vec_result_u
 end
 
-macro p(n)
-    if typeof(n) == Expr
-       println(n.args)
-    end
-    return n
-end
 
-function plusOne(x)
-    x[2, 2] += 1
+function solveModel!(model)
+    if isa(model, ModelLinear)
+        obj, vec_result_x, vec_result_u = solveLinear(model.vec_c, model.vec_b, model.mat_a)
+    elseif isa(model, ModelMixed)
+        obj, vec_result_x, vec_result_u = solveMix(model.vec_c, model.vec_b, model.mat_a)
+    else
+        throw("Wrong input model.")
+    end
+    model.solution = Solution(obj, vec_result_x, vec_result_u)
 end
